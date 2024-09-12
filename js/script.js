502aac198d1b0f7d6c7d49359969e9600c54640f
ZeynepIlkay <ilkayzeynepp@gmail.com>
2024-09-12 14:55:26 +0300

    Arayüz güncellendi

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+document.getElementById('search-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Formun sayfa yenilemesini engeller
+    var username = document.getElementById('username-input').value;
+    fetchGitHubData(username);
+});
+
 function fetchGitHubData(username) {
     fetchUserInfo(username);
     fetchGitHubProjects(username);
@@ -6,7 +12,7 @@ function fetchGitHubData(username) {
 function fetchUserInfo(username) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', `https://api.github.com/users/${username}`, true);
-   // xhr.setRequestHeader('Authorization', 'Bearer x');  // Token gerekli
+    // xhr.setRequestHeader('Authorization', 'Bearer x');  // Token gerekli
     xhr.onload = function() {
         if (xhr.status >= 200 && xhr.status < 400) {
             var user = JSON.parse(xhr.responseText);
@@ -21,6 +27,7 @@ function fetchUserInfo(username) {
 function displayUserInfo(user) {
     var userInfo = document.getElementById('user-info');
     userInfo.innerHTML = `
+        <img src="${user.avatar_url}" alt="Profile Picture" width="100" height="100">
         <p><strong>Username:</strong> ${user.name}</p>
         <p><strong>Bio:</strong> ${user.bio}</p>
         <p><strong>Followers:</strong> ${user.followers}</p>
@@ -31,11 +38,11 @@ function displayUserInfo(user) {
 function fetchGitHubProjects(username) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', `https://api.github.com/users/${username}/repos?per_page=100`, true);
-   // xhr.setRequestHeader('Authorization', 'Bearer x');  // Token gerekli
+    // xhr.setRequestHeader('Authorization', 'Bearer x');  // Token gerekli
     xhr.onload = function() {
         if (xhr.status >= 200 && xhr.status < 400) {
             var repos = JSON.parse(xhr.responseText);
-            displayProjects(repos, username);
+            displayProjects(repos);
         } else {
             console.error('Error fetching repos', xhr.status, xhr.statusText);
         }
@@ -43,7 +50,7 @@ function fetchGitHubProjects(username) {
     xhr.send();
 }
 
-function displayProjects(repos, username) {
+function displayProjects(repos) {
     var projectList = document.getElementById('project-list');
     projectList.innerHTML = '';
 
@@ -51,7 +58,5 @@ function displayProjects(repos, username) {
         var repoElement = document.createElement('li');
         repoElement.textContent = `${repo.name} - Private: ${repo.private}`;
         projectList.appendChild(repoElement);
-
     });
 }
-
